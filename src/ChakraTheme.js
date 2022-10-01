dda890189b92e930aac87df3c4efe3e1d1e27079
Grant Pennington <grant.pennington@mxdprocess.com>
2022-10-01 16:21:05 -0400

    imported my custom theme and passed it to theme wrapper

--- src/ChakraTheme.js
+++ src/ChakraTheme.js
@@ -1,9 +1,10 @@
 import * as React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
+import theme from './CustomTheme'
 
 export default function ChakraTheme({ children }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       { children }
     </ChakraProvider>
   )
