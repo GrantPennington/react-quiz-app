5089d9fcfc53e638eedae4ea9b4b89e63e037af5
Grant Pennington <grant.pennington@mxdprocess.com>
2022-10-01 22:37:01 -0400

    added modal for showing quiz details. added state for opening and closing modal.

--- src/components/QuizCard.js
+++ src/components/QuizCard.js
@@ -1,9 +1,12 @@
 import { Box, Button, Container, Divider, Heading, Popover, PopoverArrow, PopoverBody, PopoverCloseButton, PopoverContent, PopoverHeader, PopoverTrigger, Text } from '@chakra-ui/react'
-import React from 'react'
+import React, { useState } from 'react'
 import { DeleteIcon, CloseIcon, EditIcon, ChevronRightIcon } from '@chakra-ui/icons'
 import ProgressBar from './ProgressBar'
+import BasicUsage from './BasicModal'
 
 function QuizCard({ width, height, content }) {
+    const [details, setDetails] = useState()
+    const [showDetails, setShowDetails] = useState(false)
     /*
     content shape = {
         label:
@@ -12,15 +15,18 @@ function QuizCard({ width, height, content }) {
         questionCount:
         dueDate:
         timeLimit:
+        answered:
     }
     */
   return (
     <>
-        {/* CONTAINER BOX */}
+    {showDetails ? 
+        <BasicUsage update={setShowDetails} show={showDetails} content={content}/>
+        :
         <Box background={'light'} width={width} height={height} borderRadius={'md'} padding={0} display={'flex'} flexDirection={'row'}>
             <Box width={width}>
                 {/* HEADING BOX */}
-                <Box width={'100%'} height={100} background={'primary'} borderRadius={'sm'} display={'flex'} alignItems={'center'}>
+                <Box width={'100%'} height={100} background={'primaryLight'} borderRadius={'sm'} display={'flex'} alignItems={'center'}>
                     <Box>
                         <Heading ml={4}>
                             {content.label}
@@ -43,19 +49,19 @@ function QuizCard({ width, height, content }) {
                         <Text ml={4} fontSize={'1.75rem'}>
                             Questions: {content.questionCount}
                         </Text>
-                        <Text mr={8} mt={14} fontSize={'2rem'}>
-                            Progress: <ProgressBar progress={content.progress} />
+                        <Text mr={8} mt={14} fontSize={'1.5rem'}>
+                            <ProgressBar progress={content.progress} />
+                            Progress
                         </Text>
+                        
                     </Box>
                     <Box>
                         <Text ml={4} fontSize={'2rem'}>
                             Time: {content.timeLimit}
                         </Text>
-                        
                     </Box>
                 </Box>
             </Box>
-            
                 <Popover
                     placement='right'
                 >
@@ -77,15 +83,21 @@ function QuizCard({ width, height, content }) {
                         height={'100%'}
                         display={'flex'} alignItems={'center'} justifyContent={'center'} flexDir={'column'}
                     >
-                        <Button m={2} p={1} mb={16} bg={'dark'} _hover={{ borderRadius: 'md', bg: '#919191', h:75 }}>
+                        <Button m={2} p={1} mb={16} bg={'dark'} _hover={{ borderRadius: 'md', bg: '#919191' }}>
                             <Heading fontSize={'1.75rem'} color='light'>Start Quiz</Heading>
                         </Button>
                         <Divider />
-                        <Button m={2} p={1} mt={16} bg={'dark'} _hover={{ borderRadius: 'md', bg: '#919191', h:75  }}><Heading fontSize={'1.75rem'} color='light'>View Details</Heading></Button>
+                        <Button m={2} p={1} mt={16} bg={'dark'} 
+                                _hover={{ borderRadius: 'md', bg: '#919191' }}
+                                onClick={() => setShowDetails(true)}
+                        >
+                            <Heading fontSize={'1.75rem'} color='light'>View Details</Heading>
+                        </Button>
                     </Box>
                 </PopoverContent>
                 </Popover>
         </Box>
+        }
     </>
   )
 }
