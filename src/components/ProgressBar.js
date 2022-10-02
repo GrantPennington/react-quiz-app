5fdef0375b39367ec07daefa66364ef1dea47015
Grant Pennington <grant.pennington@mxdprocess.com>
2022-10-01 22:34:35 -0400

    added params to change size and color of bar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,13 +1,13 @@
-import { CircularProgress, CircularProgressLabel } from '@chakra-ui/react'
+import { Box, CircularProgress, CircularProgressLabel } from '@chakra-ui/react'
 import React from 'react'
 
-function ProgressBar({ progress }) {
+function ProgressBar({ progress, size, color }) {
   return (
-    <>
-      <CircularProgress value={progress} color='secondary' thickness={'10px'} size={'100px'}>
+    <Box>
+      <CircularProgress value={progress} color={color || 'secondary'} thickness={'10px'} size={size || '100px'} mb={4}>
           <CircularProgressLabel>{progress}%</CircularProgressLabel>
       </CircularProgress>
-    </>
+    </Box>
   )
 }
 
