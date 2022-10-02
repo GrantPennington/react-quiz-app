6f6568efbcfa19e61ae5e3845fa2b7a3db3ce542
Grant Pennington <grant.pennington@mxdprocess.com>
2022-10-02 19:00:52 -0400

    added a file for contants. added CREATE_QUIZ_INPUT_DATA

---
+++ src/data/constants.js
@@ -0,0 +1,72 @@
+export const CREATE_QUIZ_INPUT_DATA = [
+    {
+        id: 'quiz-title-input',
+        placeholder: 'Quiz Title',
+        heading: 'Title',
+        field: 'title',
+    },
+    {
+        id: 'quiz-topic-input',
+        placeholder: 'Quiz Topic',
+        heading: 'Topic',
+        field: 'topic',
+    },
+    {
+        id: 'quiz-due-date-input',
+        placeholder: 'Quiz Due Date',
+        heading: 'Due Date',
+        field: 'dueDate',
+    },
+    {
+        id: 'quiz-details-input',
+        placeholder: 'Quiz Details',
+        heading: 'Details',
+        field: 'details',
+    },
+];
+/*
+<Box key={'input-1'}>
+                        <CreateQuizInput 
+                            placeholder={'Quiz Title'}
+                            heading={'Title'}
+                            update={handleUpdate}
+                            field={'title'}
+                        />
+                    </Box>
+                    <Box key={'input-2'}>
+                        <CreateQuizInput 
+                            placeholder={'Quiz Topic'}
+                            heading={'Topic'}
+                            update={handleUpdate}
+                            field={'topic'}
+                        />
+                    </Box>
+                    <Box key={'input-3'}>
+                        <CreateQuizInput 
+                            placeholder={'Question amount'}
+                            variant="filled"
+                            heading={'Questions'}
+                            update={handleUpdate}
+                            field={'questionCount'}
+                        />
+                    </Box>
+                    <Box key={'input-4'}>
+                        <CreateQuizInput 
+                            placeholder={'Quiz Due Date'}
+                            heading={'Due Date'}
+                            update={handleUpdate}
+                            field={'dueDate'}
+                        />
+                    </Box>
+                    <Box key={'input-5'}>
+                        <CreateQuizInput 
+                            placeholder={'Quiz Details'}
+                            heading={'Details'}
+                            update={handleUpdate}
+                            field={'details'}
+                        />
+                    </Box>
+
+
+
+*/
